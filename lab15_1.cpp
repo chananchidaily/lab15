f7b80451bb20d4530d20d1b90a08bc7b15a8644b
tonkhao <chananchida_tha@cmu.ac.th>
2023-02-04 22:56:52 +0700

    seeedla

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
-void insertionSort(T d[],int N){
-
+template <typename T> 
+void insertionSort(T d[],int N){ 
+    for(int i=1;i<N;i++){ 
+		cout << "Pass " << i << ":"; 
+		int j= i-1; 
+    	T key = d[i]; 
+    	while(d[j]<key && j>=0) 
+    	{  
+           d[j+1]=d[j]; 
+           j--; 
+    	}
+       d[j+1]=key; 
+		for(int j=0;j < N;j++){  
+			cout << d[j] ; 
+			if(j!=N-1) cout << " "; 
+		}
+		cout << endl; 
+	}
 }
 
 int main(){
@@ -15,5 +30,7 @@ int main(){
 	insertionSort(a,10);
 	
 	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	for(int i = 0; i < 10; i++) 
+	cout << a[i] << " ";	
 }
+
